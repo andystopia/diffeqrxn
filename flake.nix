e7a7a696955c1564758aea7597b7e9c82024d376
andystopia <91233633+andystopia@users.noreply.github.com>
2024-07-11 14:15:59 -0400

    nix flake derivation written

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
 
+  nixConfig = {
+    allowBroken = true;
+  };
+
   outputs = {
     self,
     nixpkgs,
@@ -20,7 +24,60 @@
     # Provide some binary packages for selected system types.
     packages = forAllSystems (system: let
       pkgs = nixpkgsFor.${system};
+        compiler = "ghc94";
+
+        hpkgs = pkgs.haskell.packages.${compiler}.override {
+          overrides = hself: hsuper: {
+
+            # some packages are not yet available / lack the 
+            # necessary version within the nixpkgs, so we'll just
+            # download these special cases from hackage.
+            effectful = pkgs.haskell.lib.overrideCabal hsuper.effectful {
+              version = "2.3.1.0";
+              sha256 = "sha256-A85wBg/FOFXJ7fDhdK3nNb5bLMAm6ESZo7rrZsdk/IU=";
+            };
+
+            effectful-core = pkgs.haskell.lib.overrideCabal hsuper.effectful-core {
+              version = "2.3.1.0";
+              sha256 = "sha256-NfqWA7z0oq/trtKMegqDX9ObnZPqeC18j6rx907swU4=";
+            };
+
+            effectful-plugin =  pkgs.haskell.lib.overrideCabal hsuper.effectful-plugin {
+              version = "1.1.0.3";
+              sha256 = "sha256-AVIQq06Oy9XUR7gI2Q0b3xPKSj+zPdJuqDgpAxI8oB8=";
+            };
+
+            megaparsec =  pkgs.haskell.lib.overrideCabal hsuper.megaparsec {
+              version = "9.6.1";
+              sha256 = "sha256-oyJXvoR9vnGsItUSqxFfnOJvoIvNvqfKi1JWoXANy/8=";
+              editedCabalFile = null; 
+              revision = null;
+            };
+
+            diagnose = pkgs.lib.pipe hsuper.diagnose [
+              # doJailbreak means "yeah, you can just ignore
+              # this package's version constraints"
+
+              # this is *probably* okay: https://github.com/Mesabloo/diagnose/issues/18
+              # the reason this is marked as broken is because the text version is 
+              # to old for this, but it works fine with a new text version too, just
+              # nobody has PR'd away the upper bound
+              pkgs.haskell.lib.doJailbreak 
+              # now that we told it to just use the current version of text, 
+              # we'll just mark it as not broken
+              pkgs.haskell.lib.markUnbroken
+              # we want the megaparsec-compat flag enabled on this package
+              # for pretty errors when we're using megaparsec
+              (pkgs.lib.trivial.flip (pkgs.haskell.lib.appendConfigureFlags) ["-f megaparsec-compat"])
+              # and then we want it to use our megaparsec 9.6.1, because it
+              # isn't in the nixpkgs default package definition.
+              (pkgs.lib.trivial.flip (pkgs.haskell.lib.addBuildDepends) [ hself.megaparsec ])
+            ];
+            
+          };
+        };
     in {
+      default = hpkgs.developPackage { root = ./.; };
     });
 
     # Add dependencies that are only needed for development
